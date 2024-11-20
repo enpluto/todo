c70e95090c91b3621d7674885cedc9686507023f
Finn Mao <loverainys@gmail.com>
2024-11-20 15:50:27 +0800

    refactor: remove layout styles from App to Layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,7 @@
-import Login from "./pages/login";
-import SignUp from "./pages/signup";
-import { useState } from "react";
+import Layout from "./Layout";
 
 function App() {
-  const [page, setPage] = useState("login");
-
-  const SwitchPage = () => {
-    return page === "login" ? (
-      <Login setPage={setPage} />
-    ) : (
-      <SignUp setPage={setPage} />
-    );
-  };
-
-  return (
-    <main className="flex flex-col md:flex-row justify-evenly">
-      <div className="flex flex-col gap-y-4 justify-center">
-        <div className="flex justify-center">
-          <img src="src/assets/check.svg" alt="" width="46" />
-          <span className="text-[32px] font-bold font-baloo">
-            ONLINE TODO LIST
-          </span>
-        </div>
-        <img
-          src="src/assets/image.svg"
-          alt=""
-          width="386"
-          className="hidden md:block"
-        />
-      </div>
-      {SwitchPage()}
-    </main>
-  );
+  return <Layout />;
 }
 
 export default App;
