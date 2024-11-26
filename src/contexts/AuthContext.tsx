fd5f550dc5f5abea89cf1758309be8a05e50206a
Finn Mao <loverainys@gmail.com>
2024-11-26 10:58:52 +0800

    feat: add AuthContext to manage user email and token

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { createContext, ReactNode, useState } from "react";
+
+interface AuthContextType {
+  token: string | null;
+  setToken: React.Dispatch<React.SetStateAction<string | null>>;
+  email: string | null;
+  setEmail: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [token, setToken] = useState<string | null>(null);
+  const [email, setEmail] = useState<string | null>(null);
+
+  return (
+    <AuthContext.Provider value={{ token, setToken, email, setEmail }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
