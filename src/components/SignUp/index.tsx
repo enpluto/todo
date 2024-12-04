ce8ac967537f3d88290b0cb928ce1e97f6bb19f8
Finn Mao <loverainys@gmail.com>
2024-12-04 17:05:57 +0800

    refactor: extract input data and improve type validation

--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import { inputDataset } from "./data";
 
 type SignUpProps = {
   setPage: React.Dispatch<React.SetStateAction<string>>;
@@ -11,8 +12,14 @@ const SignUp = ({ setPage, setEmail }: SignUpProps) => {
     handleSubmit,
     getValues,
     formState: { errors },
-  } = useForm();
-
+  } = useForm({
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
   const onSubmit = async (data) => {
     const baseUrl = "https://todolist-api.hexschool.io";
 
@@ -42,6 +49,30 @@ const SignUp = ({ setPage, setEmail }: SignUpProps) => {
     }
   };
 
+  const initInputs = inputDataset.map((input) => {
+    const { htmlFor, labelName, placeholder, id, type, validation } = input;
+
+    return (
+      <div className="flex flex-col gap-y-1" key={id}>
+        <label className="text-sm font-bold" htmlFor={htmlFor}>
+          {labelName}
+        </label>
+        <input
+          className="rounded-custom px-4 py-3"
+          placeholder={placeholder}
+          id={id}
+          type={type}
+          {...register(id, validation)}
+        />
+        {errors[id] && (
+          <span className="text-error font-bold text-sm">
+            {errors[id]?.message}
+          </span>
+        )}
+      </div>
+    );
+  });
+
   return (
     <form
       className="flex flex-col gap-y-6 mx-auto"
@@ -50,68 +81,8 @@ const SignUp = ({ setPage, setEmail }: SignUpProps) => {
       <span className="text-xl md:text-2xl text-center md:text-left font-bold pt-4 md:pt-0">
         註冊帳號
       </span>
-      {/* form */}
-      <ol className="flex flex-col gap-y-4">
-        <div className="flex flex-col gap-y-1">
-          <label className="text-sm font-bold" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="rounded-custom px-4 py-3"
-            placeholder="請輸入Email"
-            id="email"
-            type="email"
-            {...register("email", {
-              required: "此欄位不可為空",
-              pattern: {
-                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.com$/,
-                message: "Email格式錯誤",
-              },
-            })}
-          />
-          {errors.email && (
-            <span className="text-error font-bold text-sm">
-              {errors.email.message}
-            </span>
-          )}
-        </div>
-        <div className="flex flex-col gap-y-1">
-          <label className="text-sm font-bold" htmlFor="name">
-            您的暱稱
-          </label>
-          <input
-            className="rounded-custom px-4 py-3"
-            placeholder="請輸入您的暱稱"
-            id="name"
-            type="text"
-            {...register("nickname", { required: "此欄位不可為空" })}
-          />
-          {errors.nickname && (
-            <span className="text-error font-bold text-sm">
-              {errors.nickname.message}
-            </span>
-          )}
-        </div>
-        <div className="flex flex-col gap-y-1">
-          <label className="text-sm font-bold" htmlFor="password">
-            密碼
-          </label>
-          <input
-            className="rounded-custom px-4 py-3"
-            placeholder="請輸入密碼"
-            id="password"
-            type="password"
-            {...register("password", {
-              required: "此欄位不可為空",
-              minLength: { value: 6, message: "長度不足6個字" },
-            })}
-          />
-          {errors.password && (
-            <span className="text-error font-bold text-sm">
-              {errors.password.message}
-            </span>
-          )}
-        </div>
+      <div className="flex flex-col gap-y-4">
+        {initInputs}
         <div className="flex flex-col gap-y-1">
           <label className="text-sm font-bold" htmlFor="confirmPassword">
             再次輸入密碼
@@ -130,11 +101,11 @@ const SignUp = ({ setPage, setEmail }: SignUpProps) => {
           />
           {errors.confirmPassword && (
             <span className="text-error font-bold text-sm">
-              {errors.confirmPassword.message}
+              {errors.confirmPassword?.message}
             </span>
           )}
         </div>
-      </ol>
+      </div>
       <button
         className="bg-black text-white rounded-custom px-12 py-3 font-bold max-w-[160px] m-auto"
         type="submit"
