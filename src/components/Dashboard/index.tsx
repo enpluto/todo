6502c31aaf326db7f4d5a4cab9b3ff707a923815
Finn Mao <loverainys@gmail.com>
2024-12-07 12:12:45 +0800

    refactor: seperate styles and login in TodoContainer

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../contexts/AuthContext";
+import { Todo, useAuth } from "../../contexts/AuthContext";
 import EmptyList from "./EmptyList";
 import NavBar from "./NavBar";
 import TodoFilter from "./TodoFilter";
 import TodoInput from "./TodoInput";
 import TodoList from "./TodoList";
+
+interface TodoContainerProps {
+  todos: Todo[];
+  activeTab: string;
+  setActiveTab: React.Dispatch<React.SetStateAction<string>>;
+}
 
 const Dashboard = () => {
   const [activeTab, setActiveTab] = useState("全部");
@@ -18,20 +24,34 @@ const Dashboard = () => {
     }
   }, [state.token]);
 
+  const TodoContainer = ({
+    todos,
+    activeTab,
+    setActiveTab,
+  }: TodoContainerProps) => {
+    const hasTodo = todos.length > 0;
+
+    return hasTodo ? (
+      <div className="bg-white rounded-custom shadow-custom">
+        <TodoFilter activeTab={activeTab} setActiveTab={setActiveTab} />
+        <TodoList activeTab={activeTab} />
+      </div>
+    ) : (
+      <EmptyList />
+    );
+  };
+
   return (
     <main className="bg-custom-gradient min-h-screen justify-end">
       <div className="max-w-[375px] md:max-w-[1028px] mx-auto flex flex-col gap-y-4 md:gap-y-10 pt-4 w-full">
         <NavBar />
         <div className="flex flex-col gap-y-4 max-w-[311px] md:max-w-[500px] w-full mx-auto">
           <TodoInput />
-          {todos.length > 0 ? (
-            <div className="bg-white rounded-custom shadow-custom">
-              <TodoFilter activeTab={activeTab} setActiveTab={setActiveTab} />
-              <TodoList activeTab={activeTab} />
-            </div>
-          ) : (
-            <EmptyList />
-          )}
+          <TodoContainer
+            todos={todos}
+            activeTab={activeTab}
+            setActiveTab={setActiveTab}
+          />
         </div>
       </div>
     </main>
