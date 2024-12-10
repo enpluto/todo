f9aed77eb6e3f1611da7d422c8fa444a73bdcbc0
Finn Mao <loverainys@gmail.com>
2024-12-10 18:28:24 +0800

    refactor: replace BrowserRouter with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Layout from "./components/Layout";
 import AuthProvider from "./contexts/AppContext";
@@ -6,12 +6,12 @@ import AuthProvider from "./contexts/AppContext";
 function App() {
   return (
     <AuthProvider>
-      <BrowserRouter basename="/todo">
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   );
 }
